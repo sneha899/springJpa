94c6590d2dfa4e951d5c8e1a6443af26d4dae676
reema ambigalla <reema ambigalla@DESKTOP-F51LJ73>
2020-05-09 14:16:42 +0530

    commit

---
+++ src/com/cg/pl/Client.java
@@ -0,0 +1,32 @@
+package com.cg.pl;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cg.bean.Employee;
+import com.cg.bean.SBU;
+
+public class Client {
+public static void main(String[] args) {
+	ApplicationContext context=new ClassPathXmlApplicationContext("bean2.xml");
+	SBU sbu=context.getBean("sbu",SBU.class);
+	System.out.println("SBU Details");
+	System.out.println("----------------------------");
+	System.out.println("sbuCode="+sbu.getSbuCode()+", sbuHead="+sbu.getSbuHead()+", sbuName="+sbu.getSbuName());
+	System.out.println("Employee Details:----------------");
+	System.out.print("[");
+	List<Employee> emp=sbu.getEmplist();
+	for(Employee data:emp){
+		System.out.print("Employee [");
+		System.out.print("empId="+data.getEmployeeId()+", ");
+		System.out.print("empName="+data.getEmployeeName()+", ");
+		System.out.print("empSalary="+data.getSalary()+"]");
+		if(emp.indexOf(data)!=emp.size()-1){
+			System.out.print(",");
+		}
+	}
+	System.out.print("]");
+}
+}
